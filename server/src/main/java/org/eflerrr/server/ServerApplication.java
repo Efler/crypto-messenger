5c884ecae14642a0181d2303acb6e7dfc7e7efd2
eflerrr <cool.yan_sv@mail.ru>
2024-05-19 06:54:31 +0300

    server module initial commit

---
+++ server/src/main/java/org/eflerrr/server/ServerApplication.java
@@ -0,0 +1,21 @@
+package org.eflerrr.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class ServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServerApplication.class, args);
+
+        log.info("/-----------------------------------------\\");
+        log.info("|------ SERVER STARTED SUCCESSFULLY ------|");
+        log.info("\\-----------------------------------------/");
+
+    }
+
+}
